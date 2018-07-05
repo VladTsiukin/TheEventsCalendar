6f21f443fafd146bbb8fd72834cf577fab5d53f5
vladislav <32671668+VladTsiukin@users.noreply.github.com>
2018-07-05 16:13:09 +0300

    add time converter to string

--- EventPlanning/wwwroot/js/site.js
+++ EventPlanning/wwwroot/js/site.js
@@ -61,3 +61,11 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
+// add zero to the digit if necessary
+function addZero(val) {
+    if (val >= 10) {
+        return val;
+    } else {
+        return '0' + val;
+    };
+}
