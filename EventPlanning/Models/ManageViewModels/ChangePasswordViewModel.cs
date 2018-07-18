c44a77241c0aaeea21a171b0bd2a4b925d5ff0f6
vladislav <32671668+VladTsiukin@users.noreply.github.com>
2018-07-18 15:39:03 +0300

    add 'change password' resources

--- EventPlanning/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ EventPlanning/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -10,18 +10,18 @@ namespace EventPlanning.Models.ManageViewModels
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Текущий пароль")]
+        [Display(Name = "Current password")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "{0} должен быть не менее {2} и не более {1} символов.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} must be at least {2} and not more than {1} characters.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Новый пароль")]
+        [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Подтвердить пароль")]
-        [Compare("NewPassword", ErrorMessage = "'Подтверждённый пароль' не совпадает с паролем выше.")]
+        [Display(Name = "Confirm password")]
+        [Compare("NewPassword", ErrorMessage = "The 'Confirmed Password' does not match the password above.")]
         public string ConfirmPassword { get; set; }
 
         public string StatusMessage { get; set; }
